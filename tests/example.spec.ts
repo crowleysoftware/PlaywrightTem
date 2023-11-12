bb49fac3f1288eb1af61fffaed7d305d99a1004c
Bob <bob@crowley.software>
2023-11-12 13:18:41 -0500

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,28 +1,39 @@
-import { test, expect, Page, request, APIRequestContext } from '@playwright/test';
-import { ChallengePage } from '../page-models/challenge-page';
+import {
+  test,
+  expect,
+  Page,
+  request,
+  APIRequestContext,
+} from "@playwright/test";
+import { ChallengePage } from "../page-models/challenge-page";
 
-test.describe('submit-welcome-challenge', async () => {
-  
+test.describe("submit-welcome-challenge", async () => {
   let page: Page;
-  let challengePage: ChallengePage;  
+  let challengePage: ChallengePage;
 
   test.beforeAll(async ({ browser }) => {
-    //call api to reset the database
-    const ctx: APIRequestContext = await request.newContext();
-    ctx.post('https://localhost:5001/api/reset');
-    
+    await resetDatabase();
+
     page = await browser.newPage();
     challengePage = new ChallengePage(page);
     await challengePage.goto();
   });
 
-  test('submit-incorrect-welcome-challenge', async () => {   
+  test.afterAll(async () => {
+    await resetDatabase();
+  });
+
+  test("submit-incorrect-welcome-challenge", async () => {
     await challengePage.submtIncorrectWelcomeChallenge();
   });
 
-  test('submit-correct-welcome-challenge', async () => {   
+  test("submit-correct-welcome-challenge", async () => {
     await challengePage.submitCorrectWelcomeChallenge();
   });
-
 });
 
+async function resetDatabase() {
+   //call api to reset the database
+   const ctx: APIRequestContext = await request.newContext();
+   ctx.post('http://localhost:5258/reset');
+}
