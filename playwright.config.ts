cc8c0f79dd9e741df3b92ba62fe1611ae958a05e
Bob <bob@crowley.software>
2023-11-17 11:19:37 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
- //require('dotenv').config();
+//require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
+
+  // Glob patterns or regular expressions to ignore test files.
+  //testIgnore: '*test-assets',
+
+  // Glob patterns or regular expressions that match test files.
+  //testMatch: '*todo-tests/*.spec.ts',
 
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -25,27 +31,30 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  
+  reporter: "html",
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   projects: [
-    { 
-      name: 'setup', 
-      testMatch: /.*\.setup\.ts/ ,
-     // use: { video: 'on' }
+    {
+      name: "setup",
+      testMatch: /.*\.setup\.ts/,
+      // use: { video: 'on' }
     },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] ,storageState: 'playwright/.auth/user.json'},
-     // dependencies: ['setup']
+      name: "firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+        storageState: "playwright/.auth/user.json",
+      },
+      // dependencies: ['setup']
     },
     /* Test against mobile viewports. */
     // {
